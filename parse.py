77221ba4f644bc85b1f1f2f89d462e7b44536b85
Estevo U. C. Castro <euccastro@gmail.com>
2014-04-21 04:11:39 +0200

    parse booleans

--- parse.py
+++ parse.py
@@ -1,9 +1,12 @@
+#-*- coding: utf-8 -*-
+
 from rpython.rlib.parsing.ebnfparse import parse_ebnf, make_parse_function
 from rpython.rlib.parsing.tree import Symbol, Nonterminal, RPythonVisitor
 from rpython.rlib.parsing.parsing import ParseError
 
 # cf. R-1RK sec. 2.1
 grammar = r"""
+    BOOLEAN: "#t|#f";
     IDENTIFIER: "\+|\-|[a-zA-Z!$%&\*/:<=>\?@\^_~][a-zA-Z0-9!$%&\*\+\-/:<=>\?@\^_~]*";
     NUMBER: "\-?(0|[1-9][0-9]*)(\.[0-9]+)?([eE][\+\-]?[0-9]+)?";
     STRING: "\"([^\"]|\\\")*\"";
@@ -12,7 +15,7 @@ grammar = r"""
     expr: <list> | <dotted_list> | <atom>;
     list: ["("] >sequence< [")"];
     dotted_list: ["("] >sequence< ["."] expr [")"];
-    atom: <STRING> | <NUMBER> | <IDENTIFIER> | <nil>;
+    atom: <BOOLEAN> | <STRING> | <NUMBER> | <IDENTIFIER> | <nil>;
     nil: ["("] [")"];
     """
 
@@ -20,7 +23,7 @@ regexs, rules, ToAST = parse_ebnf(grammar)
 parse = make_parse_function(regexs, rules, eof=True)
 
 try:
-    ast = parse('($one "one" (6 () . ("5"))) 2; This is a comment.\n 2.5 ( 3 3.5 . "rest" ) \n"4.6" (1) (3 . ()) (4 ()) ')
+    ast = parse('($one #t (#f () . ("5"))) 2; This is a comment.\n 2.5 ( 3 3.5 . "rest" ) \n"4.6" (1) (3 . ()) (4 ()) ')
     ast.view()
     transformed = ToAST().transform(ast)
     transformed.view()

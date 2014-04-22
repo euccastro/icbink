a81c7ae79b2852dd38b44080b1e44ce3b08398ab
Estevo U. C. Castro <euccastro@gmail.com>
2014-04-22 04:35:51 +0200

    make basic visitor; get rid of numbers

---
+++ kernel_type.py
@@ -0,0 +1,80 @@
+from rpython.rlib import jit
+
+class KernelObject(object):
+    def equal(self, other):
+        return other is self
+    @jit.elidable
+    def eq(self, other):
+        return other is self
+    def tostring(self):
+        return str(self)
+
+#XXX: Unicode
+class String(KernelObject):
+    _immutable_fields_ = ['value']
+    def __init__(self, value):
+        assert isinstance(value, str)
+        self.value = value
+    @jit.elidable
+    def tostring(self):
+        return '"%s"' % self.value
+
+#XXX: Unicode
+class Symbol(KernelObject):
+
+    _immutable_fields_ = ['value']
+
+    def __init__(self, value):
+        assert isinstance(value, str)
+        self.value = value
+
+    @jit.elidable
+    def tostring(self):
+        return self.value
+
+    symbol_table = {}
+    @classmethod
+    @jit.elidable
+    def get_interned(cls, name):
+        try:
+            return cls.symbol_table[name]
+        except KeyError:
+            ret = cls.symbol_table[name] = Symbol(name)
+            return ret
+
+class Nil(KernelObject):
+    @jit.elidable
+    def tostring(self):
+        return "()"
+
+nil = Nil()
+
+class Boolean(KernelObject):
+    _immutable_fields_ = ['value']
+    def __init__(self, value):
+        assert isinstance(value, bool)
+        self.value = value
+    @jit.elidable
+    def tostring(self):
+        return '#t' if self.value else '#f'
+
+true = Boolean(True)
+false = Boolean(False)
+
+class Pair(KernelObject):
+    _immutable_fields_ = ['car', 'cdr']
+    def __init__(self, car, cdr):
+        # XXX: specialize for performance?
+        assert isinstance(car, KernelObject)
+        assert isinstance(cdr, KernelObject)
+        self.car = car
+        self.cdr = cdr
+    def tostring(self):
+        return "(%s . %s)" % (self.car.tostring(), self.cdr.tostring())
+
+class Program(KernelObject):
+    """Not a real Kernel value; just to keep RPython happy."""
+    def __init__(self, exprs):
+        self.exprs = exprs
+    def tostring(self):
+        return str([expr.tostring() for expr in self.exprs])
--- parse.py
+++ parse.py
@@ -1,32 +1,62 @@
 #-*- coding: utf-8 -*-
 
 from rpython.rlib.parsing.ebnfparse import parse_ebnf, make_parse_function
-from rpython.rlib.parsing.tree import Symbol, Nonterminal, RPythonVisitor
 from rpython.rlib.parsing.parsing import ParseError
+from rpython.rlib.parsing.tree import RPythonVisitor
 
-# cf. R-1RK sec. 2.1
+import kernel_type as kt
+
+
+# cf. R-1RK sec. 2.1, although I don't claim all regexes are correct at this
+# point.  They're just something to work with; I'll review them for correctness
+# later.
 grammar = r"""
     BOOLEAN: "#t|#f";
     IDENTIFIER: "\+|\-|[a-zA-Z!$%&\*/:<=>\?@\^_~][a-zA-Z0-9!$%&\*\+\-/:<=>\?@\^_~]*";
-    NUMBER: "\-?(0|[1-9][0-9]*)(\.[0-9]+)?([eE][\+\-]?[0-9]+)?";
     STRING: "\"([^\"]|\\\")*\"";
     IGNORE: " |\n|;[^\n]*\n";
     sequence: expr >sequence< | expr;
     expr: <list> | <dotted_list> | <atom>;
     list: ["("] >sequence< [")"];
     dotted_list: ["("] >sequence< ["."] expr [")"];
-    atom: <BOOLEAN> | <STRING> | <NUMBER> | <IDENTIFIER> | <nil>;
+    atom: <BOOLEAN> | <STRING> | <IDENTIFIER> | <nil>;
     nil: ["("] [")"];
     """
+
+class Visitor(RPythonVisitor):
+    def visit_sequence(self, node):
+        return kt.Program([self.dispatch(c) for c in node.children])
+    def visit_BOOLEAN(self, node):
+        return kt.true if node.token.source == "#t" else kt.false
+    def visit_IDENTIFIER(self, node):
+        return kt.Symbol.get_interned(node.token.source)
+    def visit_STRING(self, node):
+        # Remove quotation marks.
+        return kt.String(node.token.source[1:-1])
+    def visit_list(self, node):
+        return build_pair_chain([self.dispatch(c) for c in node.children]
+                                + [kt.nil])
+    def visit_dotted_list(self, node):
+        return build_pair_chain([self.dispatch(c) for c in node.children])
+    def visit_nil(self, node):
+        return kt.nil
+
+def build_pair_chain(lst, start=0):
+    end = len(lst)
+    if end - start == 2:
+        return kt.Pair(lst[start], lst[start+1])
+    else:
+        return kt.Pair(lst[start], build_pair_chain(lst, start+1))
 
 regexs, rules, ToAST = parse_ebnf(grammar)
 parse = make_parse_function(regexs, rules, eof=True)
 
 try:
-    ast = parse('($one #t (#f () . ("5"))) 2; This is a comment.\n 2.5 ( 3 3.5 . "rest" ) \n"4.6" (1) (3 . ()) (4 ()) ')
-    ast.view()
+    ast = parse('($one #t (#f () . ("5"))) "2"; This is a comment.\n"2.5" ( "3" "3.5" . "rest" ) \n"4.6" ("1") ("3" . ()) ("4" ()) ')
+    #ast.view()
     transformed = ToAST().transform(ast)
-    transformed.view()
+    #transformed.view()
+    program = Visitor().dispatch(transformed)
 except ParseError as e:
     print e.nice_error_message()
 import pdb

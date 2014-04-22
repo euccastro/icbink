a0d5bbb90336e98829cf2a25e72dce7064b9e95d
Estevo U. C. Castro <euccastro@gmail.com>
2014-04-22 08:02:57 +0200

    enable JIT

--- interpret.py
+++ interpret.py
@@ -1,3 +1,5 @@
+from rpython.rlib import jit
+
 import continuation
 import environment
 import parse
@@ -7,10 +9,19 @@ def run_one_expr(val, env):
     cont = continuation.TerminalContinuation()
     try:
         while True:
+            driver.jit_merge_point(val=val, env=env, cont=cont)
             val, env, cont = val.interpret(env, cont)
     except continuation.Done as e:
         return e.value
 
+def get_printable_location(green_val):
+    if green_val is None:
+        return "No green val"
+    else:
+        return green_val.tostring()
+driver = jit.JitDriver(reds=['env', 'cont'],
+                       greens=['val'],
+                       get_printable_location=get_printable_location)
 
 if __name__ == '__main__':
     expr, = parse.parse('"test"').exprs

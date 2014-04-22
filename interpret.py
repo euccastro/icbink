5ab379b5b770566224e89decb573a49a13dba3e3
Estevo U. C. Castro <euccastro@gmail.com>
2014-04-22 23:54:48 +0200

    factor out kernel_type again
    But now take everything that is a KernelValue or on which these
    depend.
    
    This was done because otherwise otherwise running interpret as
    __main__ would cause two families of types.

--- interpret.py
+++ interpret.py
@@ -1,208 +1,38 @@
 from rpython.rlib import jit
 
+import kernel_type as kt
 import parse
+import primitive
 
-
-class KernelError(Exception):
-    def __init__(self, msg):
-        self.msg = msg
-
-class KernelTypeError(KernelError):
-    pass
-
-class KernelValue(object):
-    simple = True
-    def equal(self, other):
-        return other is self
-    @jit.elidable
-    def eq(self, other):
-        return other is self
-    def tostring(self):
-        return str(self)
-    def interpret(self, env, cont):
-        assert self.simple
-        return cont.plug_reduce(self.interpret_simple(env))
-    def interpret_simple(self, env):
-        return self
-    def combine(self, operands, env, cont):
-        raise KernelTypeError("%s %s is not callable"
-                              % (self.__class__.__name__,
-                                 self.tostring()))
-
-#XXX: Unicode
-class String(KernelValue):
-    _immutable_fields_ = ['value']
-    def __init__(self, value):
-        assert isinstance(value, str)
-        self.value = value
-    @jit.elidable
-    def tostring(self):
-        return '"%s"' % self.value
-
-#XXX: Unicode
-class Symbol(KernelValue):
-
-    _immutable_fields_ = ['value']
-
-    def __init__(self, value):
-        assert isinstance(value, str)
-        self.value = value
-
-    @jit.elidable
-    def tostring(self):
-        return self.value
-
-    def interpret_simple(self, env):
-        return env.lookup(self)
-
-_symbol_table = {}
-
-def get_interned(name):
-    try:
-        return _symbol_table[name]
-    except KeyError:
-        ret = _symbol_table[name] = Symbol(name)
-        return ret
-
-class Nil(KernelValue):
-    @jit.elidable
-    def tostring(self):
-        return "()"
-
-nil = Nil()
-
-class Boolean(KernelValue):
-    _immutable_fields_ = ['value']
-    def __init__(self, value):
-        assert isinstance(value, bool)
-        self.value = value
-    @jit.elidable
-    def tostring(self):
-        return '#t' if self.value else '#f'
-
-true = Boolean(True)
-false = Boolean(False)
-
-class Pair(KernelValue):
-    _immutable_fields_ = ['car', 'cdr']
-    def __init__(self, car, cdr):
-        # XXX: specialize for performance?
-        assert isinstance(car, KernelValue)
-        assert isinstance(cdr, KernelValue)
-        self.car = car
-        self.cdr = cdr
-    def tostring(self):
-        return "(%s . %s)" % (self.car.tostring(), self.cdr.tostring())
-
-class Combiner(KernelValue):
-    pass
-
-class Operative(Combiner):
-    pass
-
-class Primitive(Operative):
-    def __init__(self, code):
-        self.code = code
-    def combine(self, operands, env, cont):
-        return self.code(operands, env, cont)
-
-class SimplePrimitive(Operative):
-    def __init__(self, code):
-        self.code = code
-    def combine(self, operands, env, cont):
-        return cont.plug_reduce(self.code(operands))
-
-class Applicative(Combiner):
-    def __init__(self, combiner):
-        self.combiner = combiner
-    def combine(self, operands, env, cont):
-        return evaluate_arguments(operands,
-                                  env,
-                                  ApplyContinuation(self.combiner, env, cont))
-
-class Program(KernelValue):
-    """Not a real Kernel value; just to keep RPython happy."""
-    def __init__(self, exprs):
-        self.exprs = exprs
-    def tostring(self):
-        return str([expr.tostring() for expr in self.exprs])
-
-class Continuation(KernelValue):
-    pass
-
-class NotFound(KernelError):
-    def __init__(self, val):
-        self.val = val
-
-class Environment(KernelValue):
-    def __init__(self, parents, bindings):
-        self.parents = parents
-        self.bindings = bindings
-    def lookup(self, symbol):
-        try:
-            ret = self.bindings[symbol]
-            return ret
-        except KeyError:
-            for parent in self.parents:
-                try:
-                    ret = parent.lookup(symbol)
-                    return ret
-                except NotFound:
-                    pass
-            raise NotFound(symbol)
 
 def empty_environment():
-    return Environment([], {})
+    return kt.Environment([], {})
 
 def _simple_applicative(fn):
-    return Applicative(SimplePrimitive(fn))
+    return kt.Applicative(kt.SimplePrimitive(fn))
 
-_ground_environment = Environment(
-    [],
-    {'string-append': _simple_applicative(string_append)})
+class _GroundEnvironmentContainer:
+    env = None
 
+def pythonify(s):
+    return s.replace('-', '_')
 
-class Done(Exception):
-    def __init__(self, value):
-        self.value = value
+def init_ground_environment():
+    d = dict([(kt.get_interned(name),
+               _simple_applicative(getattr(primitive, pythonify(name))))
+              for name in ['string-append']])
+    _GroundEnvironmentContainer.env = kt.Environment([], d)
 
-class TerminalCont(Continuation):
-    def plug_reduce(self, val):
-        raise Done(val)
-
-class EvalArgsCont(Continuation):
-    def __init__(self, vals, env, cont):
-        self.vals = vals
-        self.env = env
-        self.prev = cont
-    def plug_reduce(self, val):
-        return evaluate_arguments(self.vals.cdr,
-                                  self.env,
-                                  GatherArgsCont(val, self.prev))
-
-class GatherArgsCont(Continuation):
-    def __init__(self, val, prev):
-        self.val = val
-        self.prev = prev
-    def plug_reduce(self, val):
-        return self.prev.plug_reduce(Pair(self.val, val))
-
-
-class ApplyCont(Continuation):
-    def __init__(self, combiner, env, prev):
-        self.combiner = combiner
-        self.env = env
-        self.prev = prev
-    def plug_reduce(self, args):
-        return self.combiner.combine(args, self.env, self.prev)
+def standard_environment():
+    return kt.Environment([_GroundEnvironmentContainer.env], {})
 
 def run_one_expr(val, env):
-    cont = continuation.TerminalContinuation()
+    cont = kt.TerminalCont()
     try:
         while True:
             driver.jit_merge_point(val=val, env=env, cont=cont)
             val, env, cont = val.interpret(env, cont)
-    except continuation.Done as e:
+    except kt.Done as e:
         return e.value
 
 def get_printable_location(green_val):
@@ -214,15 +44,23 @@ driver = jit.JitDriver(reds=['env', 'cont'],
                        greens=['val'],
                        get_printable_location=get_printable_location)
 
-def evaluate_arguments(vals, env, cont):
-    if isinstance(vals, Pair):
-        return vals.car, env, continuation.EvalArgsCont(vals, env, cont)
-    else:
-        return cont.plug_reduce(nil)
+
+def keval(expr_str, env):
+    expr, = parse.parse(expr_str).exprs
+    return run_one_expr(expr, env)
+
+def test():
+    init_ground_environment()
+    env = standard_environment()
+    env.set(kt.get_interned('x'), kt.String('the-value-bound-by-x'))
+    for expr_str, typ, value in [('"one"', kt.String, 'one'),
+                                 ('x', kt.String, 'the-value-bound-by-x')]:
+        result = keval(expr_str, env)
+        assert isinstance(result, typ)
+        assert result.value == value
+    assert isinstance(keval('string-append', env), kt.Applicative)
+    print "All OK."
 
 if __name__ == '__main__':
-    expr, = parse.parse('"test"').exprs
-    result = run_one_expr(expr, environment.empty_environment())
-    assert isinstance(result, String)
-    assert result.value == 'test'
-    print "All OK."
+    test()
+
--- primitive.py
+++ primitive.py
@@ -1,3 +1,14 @@
-def string_append(ss):
-    #XXX: if RPython complains, make a StringBuilder.
-    return ''.join([s.value for s in ss])
+from rpython.rlib import rstring
+
+import kernel_type as kt
+
+
+def string_append(vals):
+    s = rstring.StringBuilder()
+    while vals is not kt.nil:
+        assert isinstance(car, kt.Pair)
+        car = vals.car
+        assert isinstance(car, kt.String)
+        s.append(car.value)
+        vals = vals.cdr
+    return kt.String(s.build)
